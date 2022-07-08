75cf028e2c921c1dd2564111f4ab2cf5f5f25b9c
asppj <asppj@foxmail.com>
2022-07-08 19:59:45 +0800

    button

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,20 +1,28 @@
 import { Suspense } from 'react'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route, Link } from 'react-router-dom'
 import routes, { Home, About, NotFound } from './config/routes'
 
 const Router = () => {
-  const myRoutes = routes.map((item) => {
-    return <Route key={item.path} {...item} element={<item.element/>}/>
-  })
-  return ( 
-    <HashRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          {myRoutes}
-        </Routes>
-      </Suspense>
-    </HashRouter>
-  )
+	const myRoutes = routes.map((item) => {
+		return <Route key={item.path} {...item} element={<item.element />} />
+	})
+	const navItems = routes.map((item) => {
+		return <button className="btn focus:outline-none focus:shadow-outline">
+			<Link to={item.path}>{item.name}</Link>
+		</button>
+	})
+	return (
+		<HashRouter>
+			<nav className="flex flex-col rounded">
+				{navItems}
+			</nav>
+			<Suspense fallback={<div className="bg-slate-300">Loading...</div>}>
+				<Routes>
+					{myRoutes}
+				</Routes>
+			</Suspense>
+		</HashRouter>
+	)
 }
 
 export default Router
